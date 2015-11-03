9439dc01dc66596be375983ce9135e244de714a4
George Kephart <Gkephart@cnm.edu>
2015-11-03 11:19:35 -0700

    created a new function that should chane the color of paragraph1

---
+++ public-html/javaScript/localJavaScript.js
@@ -0,0 +1,8 @@
+/**
+ * Created by George on 11/3/15.
+ */
+// this function should change the color of paragraph to red
+	function change() {
+	var changeColorP = document.getElementById('paragraph1');
+	changeColorP.style.color = 'red';
+}
